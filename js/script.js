2d02ee798ad2d28561cc1d5a8538b0d4f328af8a
udayhasan <34761590+udayhasan@users.noreply.github.com>
2018-07-09 12:37:05 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
 function login() {
-	var money=10; 
-	alert(money);
+	if( document.loginForm.user_name.value == "" )
+	{
+		alert( "Please provide User Name!" );
+		document.loginForm.user_name.focus() ;
+		return false;
+	}
+	
+	if( document.loginForm.pass.value == "" )
+	{
+		alert( "Please provide Password!" );
+		document.loginForm.pass.focus() ;
+		return false;
+	}
 }
