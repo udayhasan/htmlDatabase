4e303fde848815abff58040986dd661245da2eb3
udayhasan <udayhasan111@gmail.com>
2018-07-10 10:25:01 +0600

    Login page built

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 function login() {
 	if( document.loginForm.user_name.value == "" )
 	{
-		alert( "Please provide User Name!" );
+		document.getElementById("login_warning").innerHTML="Warning: Provide a user name first!" ;
+		document.getElementById("login_warning").style.color="red";
 		document.loginForm.user_name.focus() ;
 		return false;
 	}
 	
 	if( document.loginForm.pass.value == "" )
 	{
-		alert( "Please provide Password!" );
+		document.getElementById("login_warning").innerHTML="Warning: Provide your Password";
+		document.getElementById("login_warning").style.color="red";
 		document.loginForm.pass.focus() ;
 		return false;
 	}
